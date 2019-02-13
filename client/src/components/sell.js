4f73e1426e7affa215e376750eb1e41ef9d7d812
Tiffany Kim <minikim9@hotmail.com>
2019-02-12 20:07:47 -0800

    got s3 working

--- client/src/components/sell.js
+++ client/src/components/sell.js
@@ -4,8 +4,6 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import {createItemData} from '../actions'
 import '../assets/css/sell.scss'
-import axios from 'axios';
-import $ from 'jquery';
 // import Image from './imageUpload'
 
 
@@ -23,79 +21,15 @@ class Sell extends Component {
             description: "",
             location: "",
             image: "",
-            selectedFile: null,
-			selectedFiles: null
     
         }
     }
 
-    singleFileChangedHandler = ( event ) => {
-		this.setState({
-			selectedFile: event.target.files[0]
-        });
-        console.log("selected file", this.state)
-    };
-    
-    singleFileUploadHandler = ( event ) => {
-		const data = new FormData();
-// If file selected
-		if ( this.state.selectedFile ) {
-			data.append( 'profileImage', this.state.selectedFile, this.state.selectedFile.name );
-			axios.post( '/api/profile/profile-img-upload', data, {
-				headers: {
-					'accept': 'application/json',
-					'Accept-Language': 'en-US,en;q=0.8',
-					'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-				}
-			})
-				.then( ( response ) => {
-					if ( 200 === response.status ) {
-						// If file size is larger than expected.
-						if( response.data.error ) {
-							if ( 'LIMIT_FILE_SIZE' === response.data.error.code ) {
-								this.ocShowAlert( 'Max size: 2MB', 'red' );
-							} else {
-								console.log( response.data );
-// If not the given file type
-								this.ocShowAlert( response.data.error, 'red' );
-							}
-						} else {
-							// Success
-							let fileName = response.data;
-							console.log( 'filedata', fileName );
-							this.ocShowAlert( 'File Uploaded', '#3089cf' );
-						}
-					}
-				}).catch( ( error ) => {
-				// If another error
-				this.ocShowAlert( error, 'red' );
-			});
-		} else {
-			// if file not selected throw error
-			this.ocShowAlert( 'Please upload file', 'red' );
-		}
-    };
-    
-    ocShowAlert = ( message, background = '#3089cf' ) => {
-		let alertContainer = document.querySelector( '#oc-alert-container' ),
-			alertEl = document.createElement( 'div' ),
-			textNode = document.createTextNode( message );
-		alertEl.setAttribute( 'class', 'oc-alert-pop-up' );
-		$( alertEl ).css( 'background', background );
-		alertEl.appendChild( textNode );
-		alertContainer.appendChild( alertEl );
-		setTimeout( function () {
-			$( alertEl ).fadeOut( 'slow' );
-			$( alertEl ).remove();
-		}, 3000 );
-	};
 
-
-    
     handleForm = async (event) => {
-        debugger;
-        const {createItemData,history} = this.props
-        await createItemData(event.First + " " + event.last, new Date().toISOString().substring(0, 10), event.description, Number(event.price), event.location, "iasdfasdf", event.item, event.contact);
+        // debugger;
+        const {createItemData, history} = this.props
+        await createItemData(event.First + " " + event.last, new Date().toISOString().substring(0, 10), event.description, Number(event.price), event.location, event.item, event.contact);
         
         // const data = {
         //     name: event.First + " " + event.last,
@@ -106,7 +40,7 @@ class Sell extends Component {
         
         // createItem()
         
-        history.push('/item');
+        history.push(`/image/itemID/${this.props.itemID}`);
     }
     renderInput = ({
         input,
@@ -129,9 +63,7 @@ class Sell extends Component {
     }
 
     render(){
-        // console.log("this is your state in render", this.state)
-        console.log("your props in sell", this.props);
-        console.log("state of sell ", this.state)
+        console.log("this is your state in render", this.props)
         const { handleSubmit } = this.props
         return (
             <div className="container">
@@ -169,28 +101,11 @@ class Sell extends Component {
 
                     <div className="row justify-content-center">
                         <div className="but-container">
-                            <button className="submitBut btn btn-lg btn-block">Submit</button>
+                            <button type="submit" className="submitBut btn btn-lg btn-block">Submit</button>
                         </div>
                     </div>
                 </form>
                 
-				
-                <div id="oc-alert-container"></div>
-				{/* Single File Upload*/}
-				<div className="card border-light mb-3 mt-5" style={{ boxShadow: '0 5px 10px 2px rgba(195,192,192,.5)' }}>
-					<div className="card-header">
-						<h3 style={{ color: '#555', marginLeft: '12px' }}>Single Image Upload</h3>
-						<p className="text-muted" style={{ marginLeft: '12px' }}>Upload Size: 250px x 250px ( Max 2MB )</p>
-					</div>
-					<div className="card-body">
-						<p className="card-text">Please upload an image for your profile</p>
-						<input type="file" onChange={this.singleFileChangedHandler}/>
-						<div className="mt-5">
-							<button className="btn btn-info" onClick={this.singleFileUploadHandler}>Upload!</button>
-						</div>
-					</div>
-				</div>
-                <div className="space"></div>
             </div>
         )
     }
@@ -217,7 +132,8 @@ function validate({ First,  last, item, description, price, contact, location}){
 function mapStateToProps(state) {
     const { item } = state
     return {
-        data: item
+        data: item,
+        itemID: item.single
     }
 }
 
