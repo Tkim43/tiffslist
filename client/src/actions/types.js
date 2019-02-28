02e176db24c86fd1ac727f585d1f0236ea02ba37
Tiffany Kim <minikim9@hotmail.com>
2019-02-27 17:52:07 -0800

    added endpoint for a single image

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,5 +3,6 @@ export default {
     CREATE_ITEM_DATA: 'create_item_data',
     STORE_IMAGE: 'store_image',
     GET_IMAGE: 'get_image',
-    GET_ALL_DATA: 'get_all_data'
+    GET_ALL_DATA: 'get_all_data',
+    GET_SINGLE_ITEM: 'get_single_item'
 }
